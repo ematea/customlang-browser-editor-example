a27d7ff5d8f05d93e93dcca675ab51bbc644e5ea
ematea <ematea.jp@gmail.com>
2020-07-23 17:38:13 +0900

    initialize lang service

--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,10 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+import { mylangLanguageService } from './lib/mylang-language-service';
+
+mylangLanguageService.register();
+
 ReactDOM.render(
   <React.StrictMode>
     <App />
